b6337ff3a8b71fbb36555b80e1aec78266cb9d6f
Rick Xing <oreofish@gmail.com>
2012-09-08 23:08:25 +0800

    hardcode user list can work

--- strategy.js
+++ strategy.js
@@ -45,17 +45,41 @@ if(config.auth.facebook.clientid.length) {
   ));
 } 
 
+var users = [
+    { id: 1, username: 'bob', password: 'bob', email: 'bob@example.com' }
+  , { id: 2, username: 'joe', password: 'birthday', email: 'joe@example.com' }
+];
+
+function findById(id, fn) {
+  var idx = id - 1;
+  if (users[idx]) {
+    fn(null, users[idx]);
+  } else {
+    fn(new Error('User ' + id + ' does not exist'));
+  }
+}
+
+function findByUsername(username, fn) {
+  for (var i = 0, len = users.length; i < len; i++) {
+    var user = users[i];
+    if (user.username === username) {
+      return fn(null, user);
+    }
+  }
+  return fn(null, null);
+}
+
 passport.use(new LocalStrategy(
   function(username, password, done) {
-    User.findOne({ username: username }, function (err, user) {
+    // Find the user by username.  If there is no user with the given
+    // username, or the password is not correct, set the user to `false` to
+    // indicate failure and set a flash message.  Otherwise, return the
+    // authenticated `user`.
+    findByUsername(username, function(err, user) {
       if (err) { return done(err); }
-      if (!user) {
-        return done(null, false, { message: 'Unknown user' });
-      }
-      if (!user.validPassword(password)) {
-        return done(null, false, { message: 'Invalid password' });
-      }
+      if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
+      if (user.password != password) { return done(null, false, { message: 'Invalid password' }); }
       return done(null, user);
-    });
+    })
   }
 ));
